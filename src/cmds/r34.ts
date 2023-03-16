f56236f83cd9da605b6151eccc6aee8314830d0d
tinntran <tinn6w6@gmail.com>
2023-03-16 23:45:22 +0700

    fixed r34 logic bug (i hope so)

--- src/cmds/r34.ts
+++ src/cmds/r34.ts
@@ -1,13 +1,29 @@
 import axios from 'axios'
 import discord from 'discord.js'
 
+interface Res {
+    file_url: string
+}
+
 let cooldown: string[] = []
+
+const limit = 20
+
+function getData(data: Res[], bomb: boolean) {
+    const randId = Math.round(Math.random() * limit)
+
+    return bomb
+        ? data.slice(Math.abs(randId - 5), randId).map((v: Res) => v.file_url.endsWith('.mp4')
+            ? new discord.EmbedBuilder().setTitle(v.file_url)
+            : new discord.EmbedBuilder().setImage(v.file_url)
+        )
+        : data[randId].file_url
+
+}
 
 export default async function r34(i: discord.CommandInteraction) {
     const tag = i.options.get('tag')
-    const bomb = i.options.get('bomb')
-
-    const limit = 20
+    const bomb = i.options.get('bomb')?.value as boolean
 
     if (cooldown.includes(i.user.id) && bomb) {
         await i.reply({
@@ -33,14 +49,9 @@ export default async function r34(i: discord.CommandInteraction) {
             .then((res) => {
                 const { data } = res
 
-                if (!data) return
+                if (!data || !Array.isArray(data)) return
 
-                const randId = Math.round(Math.random() * limit)
-
-                return bomb
-                    ? data.slice(Math.abs(randId - 5), randId).map((v: any) =>
-                        new discord.EmbedBuilder().setImage(v.file_url))
-                    : data[randId].file_url
+                return getData(data, bomb)
             })
             .catch(e => {
                 console.log(e)
@@ -51,20 +62,15 @@ export default async function r34(i: discord.CommandInteraction) {
             .then((res) => {
                 const { data } = res
 
-                if (!data) return
+                if (!data || !Array.isArray(data)) return
 
-                const randId = Math.round(Math.random() * limit)
-
-                return bomb
-                    ? data.slice(Math.abs(randId - 5), randId).map((v: any) =>
-                        new discord.EmbedBuilder().setImage(v.file_url))
-                    : data[randId].file_url
+                return getData(data, bomb)
             })
             .catch(e => {
                 console.log(e)
             })
 
-    if (!res) {
+    if (!res || res == '') {
         await i.reply({ content: 'Don\'t have the sauce 😭', ephemeral: true })
     } else {
         if (bomb) {
